b134d4563f854b5a920e18b61c5be2046fb43d5c
Choeles Tung <choelestung@gmail.com>
2018-05-07 00:19:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function logShout(string) {
   console.log().toUpperCase()
 }
 function logWhisper(string) {
-  console.log().toLowerCase()
+  console.log(string).toLowerCase()
 }
 function sayHiToGrandma(string) {
   if(string === string.toLowerCase()){
